25ad2250f9aa6ab2ab9040d19713f5d4ddbee167
Fredrik Lindberg <fli@shapeshifter.se>
2021-06-24 10:52:59 +0200

    fix(helm): fix appVersion tag, should be prefixed with a 'v'
    The releases are named with a 'v' prefix.

--- scripts/bump-helm.cjs
+++ scripts/bump-helm.cjs
@@ -2,11 +2,11 @@ const YAML = require('yaml')
 
 module.exports.readVersion = function (contents) {
   const yaml = YAML.parse(contents)
-  return yaml.appVersion
+  return yaml.appVersion.slice(1)
 }
 
 module.exports.writeVersion = function (contents, version) {
   const yaml = YAML.parse(contents)
-  yaml.appVersion = version
+  yaml.appVersion = `v${version}`
   return YAML.stringify(yaml)
 }
