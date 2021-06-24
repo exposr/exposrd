07f008ee7e414f3723fa53f114d5ac9335ebe31a
Fredrik Lindberg <fli@shapeshifter.se>
2021-06-24 11:33:55 +0200

    build: use git describe to get branch reference

--- scripts/build-env.sh
+++ scripts/build-env.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 echo EXPOSR_BUILD_VERSION=$(git describe --tags --always --dirty 2> /dev/null || git rev-parse --short HEAD)
-echo EXPOSR_BUILD_GIT_BRANCH=$(git branch --show-current)
+echo EXPOSR_BUILD_GIT_BRANCH=$(git describe --all --always)
 echo EXPOSR_BUILD_GIT_COMMIT=$(git rev-parse HEAD)
 echo EXPOSR_BUILD_DATE=$(date -u +"%Y-%m-%dT%H:%M:%SZ")
 echo EXPOSR_BUILD_USER=$(id -u)
