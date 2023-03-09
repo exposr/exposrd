13c6d4b7ab07482c62bdbb30c798becaebeb7f1f
Fredrik Lindberg <fli@fli.dev>
2023-03-09 19:21:11 +0100

    fix(storage): update redis storage provider
    Overhaul redis storage provider to work with node-redis 4.6.5

--- src/storage/redis-storage-provider.js
+++ src/storage/redis-storage-provider.js
@@ -5,99 +5,196 @@ import { Logger } from '../logger.js';
 class RedisStorageProvider {
     constructor(opts) {
         this.logger = Logger("redis-storage");
-        const callback = opts.callback;
         const redisUrl = opts.redisUrl;
+
         if (!redisUrl) {
             throw new Error("No Redis connection string provided");
         }
+
         this.logger.isTraceEnabled() &&
             this.logger.trace({
-                operation: 'redis_new',
+                operation: 'new',
                 url: redisUrl,
             });
-        const redis = this._client = Redis.createClient({
+
+        this._client = Redis.createClient({
             url: redisUrl.href,
-            connect_timeout: 2147483647,
         });
-        redis.once('ready', () => {
-            process.nextTick(callback);
+
+        this._client.on('ready', () => {
+            this._client.hello()
+                .catch(() => {})
+                .then((info) => {
+                    if (!info) {
+                        return;
+                    }
+                    this.logger.info({
+                        message: `connected to redis ${redisUrl}, version: ${info?.version}, client-id: ${info?.id} `,
+                        operation: 'connect',
+                        server: redisUrl,
+                        version: info?.version,
+                        clientId: info?.id,
+                    });
+                    this._client_id = info?.id;
+                    this._client_was_ready = true;
+                    delete this._client_error;
+                });
         });
-        redis.on('connect', () => {
-            this.logger.info({
-                operation: 'redis_connect',
-                url: redisUrl,
-                version: redis.server_info?.redis_version,
+
+        this._client.connect()
+            .catch((err) => {
+                this.logger.error({
+                    message: `Failed to connect to ${redisUrl}: ${err.message}`,
+                    operation: 'connect',
+                });
+                typeof opts.callback === 'function' &&
+                    process.nextTick(() => opts.callback(new Error(`failed to initialize redis storage provider`)));
+            })
+            .then(() => {
+
+                this._client.on('error', (err) => {
+
+                    if (this._client_error?.message != err?.message) {
+                        this.logger.error({
+                            message: `redis client error: ${err.message}`,
+                        });
+                        this.logger.debug({
+                            message: err.message,
+                            stack: err.stack
+                        });
+
+                        this._client_error = err;
+                    }
+
+                    if (!this._client.isReady && this._client_was_ready) {
+                        this.logger.warn({
+                            message: `disconnected from redis ${redisUrl}: ${err.message}`,
+                            operation: 'disconnect',
+                            server: redisUrl,
+                        });
+                        this._client_was_ready = false;
+                    }
+                });
+
+                typeof opts.callback === 'function' && process.nextTick(() => opts.callback());
             });
-            this.connected = true;
+    }
+
+    async destroy() {
+        if (this.destroyed) {
+            return;
+        }
+
+        this.destroyed = true;
+        this.logger.trace({
+            operation: 'destroy',
+            message: 'initiated'
         });
-        redis.on('error', (err) => {
-            this.logger.error({
-                operation: 'redis_error',
-                message: err.message,
+
+        return this._client.quit().then((res) => {
+            this.logger.trace({
+                operation: 'destroy',
+                message: 'complete',
+                res,
             });
-        });
-        redis.on('reconnecting', (obj) => {
-            this.logger.info({
-                operation: 'redis_reconnecting',
-                delay: obj.delay,
-                attempt: obj.attempt,
-            });
-            this.connected = false;
         });
     }
 
     get(key) {
         assert(key !== undefined);
-        if (!this.connected) {
+
+        if (!this._client.isReady) {
+            this.logger.error({
+                message: `failed to get '${key}': redis client not connected`,
+                operation: 'get',
+                key,
+            });
             return false;
         }
-        return new Promise((resolve, reject) => {
-            this._client.get(key, (err, data) => {
+
+        return this._client.get(key)
+            .catch((err) => {
+                this.logger.error({
+                    message: `failed to get '${key}': ${err.message}`,
+                    operation: 'get',
+                    key,
+                    err
+                });
+                return false;
+            }).then((value) => {
                 this.logger.isTraceEnabled() &&
                     this.logger.trace({
                         operation: 'get',
                         key,
-                        err,
                         data,
                     });
-                if (err) {
-                    return resolve(false);
-                }
-                resolve(data);
+                return value;
             });
-        });
     }
 
     mget(keys) {
         assert(keys !== undefined);
-        if (!this.connected) {
+
+        if (!this._client.isReady) {
+            this.logger.error({
+                message: `failed to mget '${keys.join(",")}': redis client not connected`,
+                operation: 'mget',
+                keys,
+            });
             return false;
         }
-        return new Promise((resolve, reject) => {
-            this._client.mget(keys, (err, data) => {
+
+        return this._client.MGET(keys)
+            .catch((err) => {
+                this.logger.error({
+                    message: `failed to mget '${keys.join(",")}': ${err.message}`,
+                    operation: 'mget',
+                    keys,
+                    err
+                });
+                return false;
+            }).then((data) => {
                 this.logger.isTraceEnabled() &&
                     this.logger.trace({
                         operation: 'mget',
                         keys,
-                        err,
                         data,
                     });
-                if (err) {
-                    return resolve(false);
-                }
-                resolve(data);
+                return data;
             });
-        });
     }
 
     set(key, data, opts = {}) {
         assert(key !== undefined);
         assert(data !== undefined);
-        if (!this.connected) {
+
+        if (!this._client.isReady) {
+            this.logger.error({
+                message: `failed to set '${key}': redis client not connected`,
+                operation: 'set',
+                key,
+            });
             return false;
         }
-        return new Promise((resolve, reject) => {
-            const cb = (err, res) => {
+
+        const redis_opts = {};
+        if (opts.NX == true) {
+            redis_opts.NX = true;
+        }
+        if (typeof opts.TTL == 'number') {
+            redis_opts.EX = opts.TTL;
+        }
+
+        return this._client.set(key, data, redis_opts)
+            .catch((err) => {
+                this.logger.error({
+                    message: `failed to set '${key}: ${err.message}`,
+                    operation: 'set',
+                    key,
+                    err
+                });
+                return false;
+            }).then((res) => {
                 this.logger.isTraceEnabled() &&
                     this.logger.trace({
                         operation: 'set',
@@ -105,98 +202,88 @@ class RedisStorageProvider {
                         key,
                         data,
                         res,
-                        err
                     });
-                if (err) {
-                    resolve(false);
-                } else {
-                    resolve(data);
-                }
-            }
-            const args = [key, data];
-            if (opts.NX) {
-                args.push('NX');
-            }
-            if (typeof opts.TTL == 'number') {
-                args.push('EX', `${opts.TTL}`);
-            }
-            args.push(cb)
-            this._client.set(...args);
-        });
+
+                return res ? true : false;
+            });
     };
 
     delete(key) {
         assert(key !== undefined);
-        if (!this.connected) {
+
+        if (!this._client.isReady) {
+            this.logger.error({
+                message: `failed to delete '${key}': redis client not connected`,
+                operation: 'delete',
+                key,
+            });
             return false;
         }
-        return new Promise(resolve => {
-            this._client.del(key, (res) => {
+
+        return this._client.del(key)
+            .catch((err) => {
+                this.logger.error({
+                    message: `failed to delete '${key}: ${err.message}`,
+                    operation: 'delete',
+                    key,
+                    err
+                });
+                return false;
+            }).then((res) => {
                 this.logger.isTraceEnabled() &&
                     this.logger.trace({
                         operation: 'delete',
+                        opts,
                         key,
-                        res
+                        res,
                     });
-                resolve(true);
+                return true;
             });
-        });
-    };
-
-    async destroy() {
-        if (this.destroyed) {
-            return;
-        }
-        this.destroyed = true;
-        this.connected = false;
-        this.logger.trace({
-            operation: 'destroy',
-            message: 'initiated'
-        });
-        return new Promise((resolve) => {
-            this._client.quit((res) => {
-                this.logger.trace({
-                    operation: 'destroy',
-                    message: 'complete',
-                    res,
-                });
-                resolve();
-            });
-        });
     }
 
-    list(ns, cursor, count = 10) {
-        if (this.destroyed) {
-            return {
-                cursor: 0,
-                data: [],
-            };
+    list(ns, cursor = 0, count = 10) {
+        if (!this._client.isReady) {
+            this.logger.error({
+                message: `failed to list '${ns}: redis client not connected`,
+                operation: 'list',
+                ns,
+                cursor,
+                count,
+            });
+
+            return undefined;
         }
 
-        return new Promise((resolve) => {
-            this._client.scan(cursor, 'MATCH', `${ns}*`, 'COUNT', count, (err, res) => {
-                let nextCursor;
-                let keys;
-                if (!err) {
-                    nextCursor = Number.parseInt(res[0]);
-                    keys = res[1];
-                }
-                this.logger.isTraceEnabled() && this.logger.trace({
-                    operation: 'list',
-                    count,
-                    cursor,
-                    nextCursor,
-                    numResult: keys.length,
-                    err,
-                });
-                if (err) {
-                    return resolve(undefined);
-                }
-                resolve({
-                    cursor: nextCursor,
-                    data: keys,
-                })
-            })
+        return this._client.scan(cursor, {
+            MATCH: `${ns}*`,
+            COUNT: count,
+        }).catch((err) => {
+            this.logger.error({
+                message: `failed to list '${ns}: ${err.message}`,
+                operation: 'list',
+                ns,
+                cursor,
+                count,
+                err,
+            });
+
+            return undefined;
+        }).then((res) => {
+            const nextCursor = res.cursor;
+            const keys = res.keys;
+
+            this.logger.isTraceEnabled() && this.logger.trace({
+                operation: 'list',
+                count,
+                cursor,
+                nextCursor,
+                numResult: keys.length,
+            });
+
+            return {
+                cursor: nextCursor,
+                data: keys,
+            }
         });
     }
 }
